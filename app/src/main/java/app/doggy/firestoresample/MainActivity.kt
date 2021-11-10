82d5f318cd1924e7c77e36a68766d53e127cf576
Kaito-Dogi <kendobu0405@gmail.com>
2021-11-10 13:45:39 +0900

    add: viewBindingを有効化

--- app/src/main/java/app/doggy/firestoresample/MainActivity.kt
+++ app/src/main/java/app/doggy/firestoresample/MainActivity.kt
@@ -2,10 +2,15 @@ package app.doggy.firestoresample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.doggy.firestoresample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // バインディングクラスの変数
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
