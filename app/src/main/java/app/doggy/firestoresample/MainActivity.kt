bcc61e58894df25c3a39bb02c8bb402fd12901f9
Kaito-Dogi <kendobu0405@gmail.com>
2021-11-10 14:01:11 +0900

    add: Firestoreにデータを追加

--- app/src/main/java/app/doggy/firestoresample/MainActivity.kt
+++ app/src/main/java/app/doggy/firestoresample/MainActivity.kt
@@ -2,7 +2,10 @@ package app.doggy.firestoresample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import app.doggy.firestoresample.databinding.ActivityMainBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +15,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        val db = Firebase.firestore
+
+        binding.button.setOnClickListener {
+            // Create a new user with a first and last name
+            val user = hashMapOf(
+                "nickname" to binding.nicknameEditText.text.toString(),
+                "course" to binding.courseEditText.text.toString(),
+            )
+
+            // Add a new document with a generated ID
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(ADD_TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(ADD_TAG, "Error adding document", e)
+                }
+        }
+    }
+
+    companion object {
+        private const val ADD_TAG = "add_user"
     }
 }
