e6e3f56088e78c8e390c92310c811d3e7bb8eeb7
Kaito-Dogi <kendobu0405@gmail.com>
2021-11-13 14:13:56 +0900

    add: 保存したデータをRecyclerViewで表示

--- app/src/main/java/app/doggy/firestoresample/MainActivity.kt
+++ app/src/main/java/app/doggy/firestoresample/MainActivity.kt
@@ -3,6 +3,7 @@ package app.doggy.firestoresample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import app.doggy.firestoresample.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
@@ -18,6 +19,54 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
 
         val db = Firebase.firestore
+
+        val taskAdapter = TaskAdapter()
+        binding.recyclerView.adapter = taskAdapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        db.collection("tasks")
+            .get()
+            .addOnSuccessListener { value ->
+                val tasks = ArrayList<Task>()
+                for (doc in value) {
+                    val task = doc.toObject<Task>()
+                    tasks.add(Task(
+                        id = task.id,
+                        title = task.title,
+                        date = task.date,
+                    ))
+                }
+                Log.d(READ_TAG, tasks.toString())
+                taskAdapter.submitList(tasks)
+            }
+            .addOnFailureListener { exception ->
+                Log.d(READ_TAG, "Error getting documents: ", exception)
+            }
+
+        val docRef = db.collection("tasks")
+        docRef.addSnapshotListener { value, e ->
+            val tasks = ArrayList<Task>()
+
+            if (e != null) {
+                Log.w(READ_TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (value != null) {
+                for (doc in value) {
+                    val task = doc.toObject<Task>()
+                    tasks.add(Task(
+                        id = task.id,
+                        title = task.title,
+                        date = task.date,
+                    ))
+                }
+                Log.d(READ_TAG, tasks.toString())
+                taskAdapter.submitList(tasks)
+            } else {
+                Log.d(READ_TAG, "Current data: null")
+            }
+        }
 
         binding.button.setOnClickListener {
             // Create a new task with a first and last name
@@ -35,30 +84,6 @@ class MainActivity : AppCompatActivity() {
                 .addOnFailureListener { e ->
                     Log.w(ADD_TAG, "Error adding document", e)
                 }
-
-            val docRef = db.collection("tasks")
-            docRef.addSnapshotListener { value, e ->
-                if (e != null) {
-                    Log.w(READ_TAG, "Listen failed.", e)
-                    return@addSnapshotListener
-                }
-
-                if (value != null) {
-                    val tasks = ArrayList<Task>()
-                    for (doc in value) {
-                        val task = doc.toObject<Task>()
-                        tasks.add(Task(
-                            id = task.id,
-                            title = task.title,
-                            date = task.date,
-                        ))
-                        Log.d(READ_TAG, value.toString())
-                    }
-                    binding.textView.text = tasks.toString()
-                } else {
-                    Log.d(READ_TAG, "Current data: null")
-                }
-            }
         }
     }
 
--- app/src/main/java/app/doggy/firestoresample/TaskAdapter.kt
+++ app/src/main/java/app/doggy/firestoresample/TaskAdapter.kt
@@ -1,4 +1,41 @@
 package app.doggy.firestoresample
 
-class TaskAdapter {
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import app.doggy.firestoresample.databinding.TaskListItemBinding
+
+class TaskAdapter: ListAdapter<Task, TaskViewHolder>(diffUtilItemCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = TaskListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        Log.d("read_task", "onCreateViewHolder")
+        return TaskViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        Log.d("read_task", "onBindViewHolder")
+        holder.bind(getItem(position))
+    }
+}
+
+class TaskViewHolder(
+    private val binding: TaskListItemBinding
+): RecyclerView.ViewHolder(binding.root) {
+    fun bind(task: Task) {
+        binding.titleTextView.text = task.title
+        binding.dateTextView.text = task.date
+        Log.d("read_task", "bindできたお！")
+    }
+}
+
+private val diffUtilItemCallback = object : DiffUtil.ItemCallback<Task>() {
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem == newItem
+    }
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem.id == newItem.id
+    }
 }
